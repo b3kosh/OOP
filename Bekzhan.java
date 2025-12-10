26cd7614334a72f5e0d8a84e5732a58de8d8afea
OOP <252288@astanait.edu.kz>
2025-12-10 09:06:29 +0500

    Bekzhan

---
+++ Bekzhan.java
@@ -0,0 +1,19 @@
+public class Bekzhan {
+    public static void main(String[] args) {
+
+        byte age = 17;
+        short num1 = 34;
+        int num2 = -15;
+        long num3 = 4567;
+
+        float num4 = 4.2323f;
+        double num5 = 4.132412341234;
+
+        char ch = '&';
+        String un = "Be";
+        System.out.println(un);
+
+        boolean isHappy = true;
+        System.out.println("B");
+    }
+}
