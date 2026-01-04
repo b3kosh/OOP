4e33d4c713e5a5398cb80c03363819cfb3ee021d
OOP <252288@astanait.edu.kz>
2026-01-04 15:36:25 +0500

    Упрастил код для Song и наследование

--- Song.java
+++ Song.java
@@ -1,63 +1,33 @@
 import java.util.Objects;
 
-public class Song {
-    private String title;
-    private int durSeconds;
-    private final Artist artist;
+public class Song extends MediaItem {
+    private Artist artist;
 
     public Song(String title, int durSeconds, Artist artist) {
-        this.title = title;
-        this.durSeconds = durSeconds;
+        super(title, durSeconds);
         this.artist = artist;
     }
 
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getDurSeconds() {
-        return durSeconds;
-    }
-
-    public void setDurSeconds(int durSeconds) {
-        this.durSeconds = durSeconds;
-    }
-
-    public Artist getArtist() {
-        return artist;
-    }
-
-
+    @Override
     public void play() {
-        System.out.println("Воспроизводится: " + title + " от " + artist.getName());
+        System.out.println("Воспроизведение: " + getTitle() + " - " + artist.getName());
     }
 
     @Override
     public String toString() {
-        return "Песня: \"" + title + "\" (" + durSeconds + " сек) - " + artist.getName();
+        return String.format("%-20s | %-15s | %d сек", getTitle(), artist.getName(), getDurSeconds());
     }
 
     @Override
-    public boolean equals(Object obj) {
-
-        if (this == obj) return true;
-
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        Song otherSong = (Song) obj;
-
-        return durSeconds == otherSong.durSeconds &&
-                Objects.equals(title, otherSong.title) &&
-                Objects.equals(artist, otherSong.artist);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Song)) return false;
+        Song song = (Song) o;
+        return getDurSeconds() == song.getDurSeconds() &&
+                Objects.equals(getTitle(), song.getTitle()) &&
+                Objects.equals(artist, song.artist);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(title, durSeconds, artist);
-    }
+    public int hashCode() { return Objects.hash(getTitle(), getDurSeconds(), artist); }
 }
