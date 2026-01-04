1e10290f068e2ac3a01a7788012152ffc9d37cd2
OOP <252288@astanait.edu.kz>
2026-01-04 16:16:25 +0500

    Добавил Scanner и управление

--- Main.java
+++ Main.java
@@ -1,30 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Artist artist1 = new Artist("The Weeknd", "R&B");
-        Artist artist2 = new Artist("Dua Lipa", "Pop");
-        Artist artist3 = new Artist("Imagine Dragons", "Rock");
+        Scanner sc = new Scanner(System.in);
+        Playlist myPlaylist = new Playlist("Избранное");
 
-        Song song1 = new Song("Blinding Lights", 200, artist1);
-        Song song2 = new Song("Levitating", 203, artist2);
-        Song song3 = new Song("Believer", 204, artist3);
-        Song song4 = new Song("Save Your Tears", 215, artist1);
+        // Тестовые данные
+        Artist art1 = new Artist("The Weeknd", "R&B");
+        myPlaylist.addSong(new Song("Blinding Lights", 200, art1));
+        myPlaylist.addSong(new Song("Save Your Tears", 215, art1));
 
-        Playlist playlist = new Playlist("Hits 2023");
+        while (true) {
+            System.out.println("\nМеню: 1.Показать | 2.Добавить | 3.Сортировать(А-Я) | 4.Сортировать(Время) | 5.Поиск | 0.Выход");
+            System.out.print("> ");
+            String input = sc.nextLine();
 
-        playlist.addSong(song1);
-        playlist.addSong(song2);
-        playlist.addSong(song3);
-        playlist.addSong(song4);
-
-        System.out.println(playlist);
-
-        playlist.playPlaylist();
-
-        Song song5 = new Song("Blinding Lights", 200, artist1);
-        System.out.println("Сравнение: " + song1.equals(song5));
-
-        playlist.removeSong("Believer");
-
-        playlist.playPlaylist();
+            switch (input) {
+                case "1" -> myPlaylist.showAll();
+                case "2" -> {
+                    System.out.print("Название: "); String t = sc.nextLine();
+                    System.out.print("Длительность (сек): "); int d = Integer.parseInt(sc.nextLine());
+                    System.out.print("Артист: "); String a = sc.nextLine();
+                    myPlaylist.addSong(new Song(t, d, new Artist(a, "Unknown")));
+                }
+                case "3" -> myPlaylist.sortByTitle();
+                case "4" -> myPlaylist.sortByDuration();
+                case "5" -> {
+                    System.out.print("Имя артиста: ");
+                    myPlaylist.findByArtist(sc.nextLine());
+                }
+                case "0" -> { System.out.println("Пока!"); return; }
+                default -> System.out.println("Неверный ввод.");
+            }
+        }
     }
 }
